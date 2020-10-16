f0739f7ff574bced44791befb059b602207b2086
Alexa Botello <abotello20@yahoo.com>
2020-10-16 22:16:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,6 +3,6 @@ def using_concat(my_favorite_things, more_favs)
 end 
 
 def using_insert(list_of_programming_languages, python)
-  list_of_programming_languages.insert (4, python)
+  list_of_programming_languages.insert(4, python)
 end
 
